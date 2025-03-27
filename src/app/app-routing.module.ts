afd626dd265c6709839951565bc68bdc1929719f
Keith Cordova <keithdc019@gmail.com>
2025-03-27 13:50:50 +0800

    move up the routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ const routes: Routes = [
     path: RouteEnum.HOME,
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
   },
+  {path: '', redirectTo: RouteEnum.HOME, pathMatch: 'full'},
   {
     path: '**',
     loadChildren: () => import('./shared/modules/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule),
   },
-  {path: '', redirectTo: RouteEnum.HOME, pathMatch: 'full'},
 ];
 
 @NgModule({
